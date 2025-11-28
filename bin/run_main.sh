285b81efdb8f086014cf46b69bf8995501e27525
jeremy.shen <jeremy.shen@ima-ems.com>
2025-11-28 18:00:19 +0800

    Update Run main shell

--- bin/run_main.sh
+++ bin/run_main.sh
@@ -31,4 +31,4 @@ exec python "$BASE_DIR/src/main.py" \
   --notifier_config "$BASE_DIR/res/notifier_config.yml" \
   --time_config "$BASE_DIR/res/time_condition.yml" \
   --system_config "$BASE_DIR/res/system_config.yml" \
-  --snapshot_storage_config "$BASE_DIR/res/snapshot_storage_config.yml"
+  --snapshot_storage_config "$BASE_DIR/res/snapshot_storage.yml"
